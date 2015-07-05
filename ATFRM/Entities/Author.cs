c9fedc5689f8fdce013268513a22d1a76a67b578
MigMax <maxence.martin.info@gmail.com>
2015-07-05 19:28:55 +0200

    Add book and author

--- ATFRM/Entities/Author.cs
+++ ATFRM/Entities/Author.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dal
+namespace Entities
 {
-    public class Class1
+    public class Author
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
